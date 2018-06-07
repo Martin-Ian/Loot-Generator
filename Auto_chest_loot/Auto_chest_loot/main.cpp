e1a7c50d277e879b54baba48332a95c90d5a63d1
Ian Martin <martinianthecoder@gmail.com>
2018-06-07 15:17:16 -0700

    Added hello world cout for first local commit

---
+++ Auto_chest_loot/Auto_chest_loot/main.cpp
@@ -0,0 +1,14 @@
+//Made by Ian Martin in June 2018
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
