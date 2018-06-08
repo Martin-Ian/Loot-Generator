7a9e2c1c6e1f4bd0f9eab44c5f016a543a1bcb63
Ian Martin <martinianthecoder@gmail.com>
2018-06-07 18:19:22 -0700

    Added a simple random number generator

--- Auto_chest_loot/Auto_chest_loot/main.cpp
+++ Auto_chest_loot/Auto_chest_loot/main.cpp
@@ -4,11 +4,35 @@
 #include <vector>
 #include <unordered_map>
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
+int get_Random_Value(int min, int max);
+//Simple random number generator, generates a random number between min <= rand < max
+
 int main(void)
 {
-	cout << "Hello World" << endl;
+	//Resets the random seed
+	srand(time(0));
+
+	for (int i = 0; i < 1000; i++)
+	{
+		cout << get_Random_Value(0, 100) << " ";
+	}
+
 	return 0;
+}
+
+//Random number generator
+int get_Random_Value(int min, int max)
+{
+	if (min >= max)
+	{
+		return min;
+	}
+
+	int difference = max - min;
+
+	return rand() % difference + min;
 }
