6385f3bd9041a549bf21f64991ae8d858e08b0f6
Ian Martin <martinianthecoder@gmail.com>
2018-06-08 11:19:46 -0700

    Added File parsing

--- Auto_chest_loot/Auto_chest_loot/main.cpp
+++ Auto_chest_loot/Auto_chest_loot/main.cpp
@@ -5,20 +5,44 @@
 #include <unordered_map>
 #include <cstdlib>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
 int get_Random_Value(int min, int max);
-//Simple random number generator, generates a random number between min <= rand < max
+//Simple random number generator, generates a random number between min <= randnum < max
+
+vector<string> file_parser(ifstream* file_to_parse);
+//custom file parser to collect relevent data from the loot file
 
 int main(void)
 {
 	//Resets the random seed
 	srand(time(0));
 
-	for (int i = 0; i < 1000; i++)
+	cout << "Welcome to the loot generator!" << endl;
+	cout << "Please enter the name of your loot file: -> ";
+
+	string file_name;
+	getline(cin, file_name);
+
+	ifstream file_to_open;
+
+	file_to_open.open(file_name);
+
+	if (file_to_open.good())
 	{
-		cout << get_Random_Value(0, 100) << " ";
+		vector<string> data = file_parser(&file_to_open);
+		
+		for (auto point : data)
+		{
+			cout << point << endl;
+		}
+	}
+	else
+	{
+		cout << "Error opening file. Now exiting." << endl;
+		return 0;
 	}
 
 	return 0;
@@ -27,12 +51,32 @@ int main(void)
 //Random number generator
 int get_Random_Value(int min, int max)
 {
+	//failsafe incase invalid operands
 	if (min >= max)
 	{
 		return min;
 	}
 
+	//need to modulo the stl rand() by the difference between min and max
 	int difference = max - min;
 
 	return rand() % difference + min;
+}
+
+vector<string> file_parser(ifstream* file_to_parse)
+{
+	//vector of strings to hold the info
+	vector<string> data{};
+
+	while (file_to_parse->good())
+	{
+		string current_line;
+		getline(*file_to_parse, current_line);
+		data.push_back(current_line);
+	}
+
+	//close the file
+	file_to_parse->close();
+
+	return data;
 }
